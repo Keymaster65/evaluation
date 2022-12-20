9294f8a048177aa0905257610aaf5058d0faac85
wsluyterman <wolf.sluyterman@scoop-software.de>
2022-12-20 17:39:22 +0100

    Several improvements

--- crac/entrypoint.sh
+++ crac/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PATH="$PATH:$JAVA_HOME/bin"
-
+typeset logPrefix="============================="
 _checkPoint(){
   export PATH="$PATH:$JAVA_HOME/bin"
   jcmd io.github.keymaster65.copper2go.vanilla.application.Main JDK.checkpoint
@@ -9,24 +9,35 @@ _checkPoint(){
 
 _wait(){
   typeset RC="1"
+  echo "Waiting for service ..."
   while [ "$RC" != "0" ]; do
-    echo "Waiting for service ..."
     curl --data Wolf http://localhost:59665/copper2go/3/api/twoway/2.0/Hello 2>&1 | fgrep "Hello Wolf"
     RC="$?"
   done
+  echo "$logPrefix Service found at  $(date +%H:%M:%S.%N)"
+  time curl --data Wolf http://localhost:59665/copper2go/3/api/twoway/2.0/Hello 2>&1 | fgrep "Hello Wolf"
 }
 
-#sleep 3000
-
 if [ ! -f "cr/cppath" ]; then
-  echo "Starting to warm up at $(date +%H:%M:%S.%N)"
+  echo "$logPrefix Starting to warm up container at $(date +%H:%M:%S.%N)"
   export VANILLA_APPLICATION_OPTS="-XX:CRaCCheckpointTo=cr"
-  bin/vanilla-application
-  #_wait
-  #_checkPoint
-  sleep 5 # needed for checkpoint to complete
+  bin/vanilla-application &
+  _wait
+  kill %1
+  echo "$logPrefix Starting to warm up application at $(date +%H:%M:%S.%N)"
+  bin/vanilla-application &
+  _wait
+  echo "Now warming up with 100 requests"
+  for i in {1..100}; do
+    curl --data Wolf http://localhost:59665/copper2go/3/api/twoway/2.0/Hello 2>&1 | fgrep -q "Hello Wolf"
+  done
+  _checkPoint
+  while [ ! -f "cr/cppath" ]; do
+    echo "Wait for checkpoint"
+    sleep 1
+   done
 fi
 
-echo "Starting with checkpoint at $(date +%H:%M:%S.%N)"
+echo "$logPrefix Starting with checkpoint at $(date +%H:%M:%S.%N)"
 _wait &
 java -XX:CRaCRestoreFrom=cr
